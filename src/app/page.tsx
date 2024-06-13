9608c117159c83658a4de860fcb4fc9183768937
Christian Wallervand <cwallervand@gmail.com>
2024-06-13 12:02:16 +0200

    Added leaderboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function ScoreboardPage() {
+import { getAllTimeLeaderboard } from "~/server/data";
+export default async function ScoreboardPage() {
+  const leaderboard = await getAllTimeLeaderboard();
+  console.log("leaderboard", leaderboard);
   return (
     <main>
-      <h1>Scoreboard</h1>
+      <h1>Leaderboard</h1>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Handle</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map((result) => (
+            <tr key={result.playerName}>
+              <td>{result.playerName}</td>
+              <td>{result.avgScore}</td>
+              <td>{result.avgFinishingPosition}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
