56da2d82f1ecdecb19544631e1f12dcdc42832b4
Christian Wallervand <cwallervand@gmail.com>
2024-06-19 15:19:49 +0200

    Added submitting feature for SubmitButton

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,8 +1,16 @@
-export const SubmitButton = () => (
-  <button
-    type="submit"
-    className="rounded-full  border-green-800/80 bg-green-800/80 px-5 py-2 text-white"
-  >
-    Lets go!
-  </button>
-);
+"use client";
+import { useFormStatus } from "react-dom";
+import clsx from "clsx";
+
+export const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  const className = clsx("rounded-full  px-5 py-2 text-white", {
+    "border-green-900/50 bg-green-900/50": pending,
+    "border-green-900 bg-green-900": !pending,
+  });
+  return (
+    <button type="submit" disabled={pending} className={className}>
+      Let&apos;s go!
+    </button>
+  );
+};
